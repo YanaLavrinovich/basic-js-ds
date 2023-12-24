51fe9fe089185a88d52961988003deebf8ecf5e8
Yana Laurynovich <ylaurynovich@godaddy.com>
2023-12-24 21:22:04 +0500

    feat: impl stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  list = [];
+
+  push(element) {
+    this.list.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.list.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.list[this.list.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
